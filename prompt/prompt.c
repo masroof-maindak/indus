bd13564030b62edb43ad3659536ef16474b97e84
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-07-21 14:53:54 +0500

    fix: don't free memory initialised outside the scope of this function

--- prompt/prompt.c
+++ prompt/prompt.c
@@ -15,31 +15,52 @@ char* populate_pwd(char* pwd){
 	return pwd;
 }
 
+char* shorten_home_dir(char* pwd, char* username, char* ret) {
+	if (!SHORTEN_HOME) {
+		return pwd;
+	}
+
+	size_t size = strlen(username) + 6;
+
+	if (strlen(pwd) >= size) {
+		char expect[size];
+		strcpy (expect, "/home/");
+		strcat (expect, username);
+
+		if (!strncmp (expect, pwd, size)) { // returns 0 on success
+			ret[0] = '~';
+			size_t remainingLen = strlen(pwd) - size;
+			if (remainingLen > 0){
+				char* srcPtr = pwd + size;
+				memcpy(ret + 1, srcPtr, remainingLen);
+			}
+			ret[remainingLen + 1] = '\0'; // null terminator
+			return ret;
+
+		} else {
+			printf ("PWD that brought me here: ... %s\n", pwd);
+			return pwd;
+		}
+
+	}
+
+	return pwd;
+}
+
 char* generate_prompt(char* pwd, char* username) {
 	char* ret = malloc(PATH_MAX);
+	if (ret == NULL) {
+		perror("malloc() error");
+		return NULL;
+	}
 
 	pwd = populate_pwd(pwd);
-
-	if (SHORTEN_HOME) {
-		size_t size = strlen(username) + 6;
-
-		if (strlen(pwd) < size) {
-			strcpy(ret, pwd);
-		} else {
-			char expect[size];
-			strcpy (expect, "/home/");
-			strcat (expect, username);
-
-			if (!strncmp (expect, pwd, size)) { // returns 0 on success
-				ret[0] = '~';
-				size_t remainingLen = strlen(pwd) - size;
-				if (remainingLen > 0){
-					char* srcPtr = pwd + size;
-					memcpy(ret + 1, srcPtr, remainingLen);
-				}
-			}
-		}
+	if (pwd == NULL) {
+		free(ret);
+		return NULL;
 	}
+
+	ret = shorten_home_dir(pwd, username, ret);
 
 	if (SHORTEN_PWD) {
 		// TODO: shorten `~/Documents/xyz` to `~/D/xyz`
@@ -48,9 +69,6 @@ char* generate_prompt(char* pwd, char* username) {
 	if (ADD_GIT_BRANCH) {
 		// TODO: in format: `(main)`
 	}
-
-	free(pwd);
-	pwd = NULL;
 
 	return ret;
 }
