a0c0366380a1829c2365c733484dcd658942544d
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-08-04 01:21:35 +0500

    patch: null check in copy_string function
    CHECK: fprintf vs fprintf vs perror

--- utils/utils.c
+++ utils/utils.c
@@ -4,6 +4,12 @@
 #include <string.h>
 
 char *copy_string(char *str) {
+	if (str == NULL) {
+		// fprintf(stderr, "Input string is null...\n");
+		fprintf(stderr, "%s\n", "Input string is null...");
+		// CHECK: Any reason to go for one of the aforementioned over the other?
+		return NULL;
+	}
     char *copy = malloc(strlen(str) + 1);
     if (copy == NULL) {
         perror("copy malloc() error");
