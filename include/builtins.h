265a8e7e6a4dc86726c86404607ea0c57bda0212
WajeehaIlyas <bscs22022@itu.edu.pk>
2024-08-24 11:07:23 -0700

    WIP: built-ins

--- include/builtins.h
+++ include/builtins.h
@@ -11,5 +11,8 @@ int indus_cd(char **args);
 int indus_rm(char **args);
 int indus_help(char **args);
 int indus_exit(char **args);
+int indus_mkdir(char **args);
+int indus_clear(char **args);
+int indus_whoami(char **args);
 
 #endif // BUILTINS_H
