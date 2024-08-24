5fe8d41828bb821bb88f04824ee656cf4b6760bb
WajeehaIlyas <130144303+WajeehaIlyas@users.noreply.github.com>
2024-08-24 04:26:34 -0700

    feat: cd builtin
    Co-authored-by: wajeeha <wajeeha@DESKTOP-CA2JO0J>

--- builtins/builtins.c
+++ builtins/builtins.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <unistd.h> 
 #include <stdlib.h>
+#include <pwd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
 #include "builtins.h"
+extern struct USER_INFO currentUser;
 
 char *builtins_str[]		  = {"cd", "ls", "rm", "help", "exit"};
 int (*builtins_fn[])(char **) = {indus_cd, indus_ls, indus_rm, indus_help,
@@ -18,16 +24,67 @@ int (*builtins_fn[])(char **) = {indus_cd, indus_ls, indus_rm, indus_help,
 int num_builtins() { return sizeof(builtins_str) / sizeof(builtins_str[0]); }
 
 int indus_ls(char **args) {
+
+	char *dir;
+    DIR *d;
+    struct dirent *dir_entry;
+
 	if (args[1] == NULL) {
 		// dir = pwd
+		 dir = get_pwd(); 
+        if (dir == NULL) {
+            perror("get_pwd() error");
+            return 1;
+        }
+	} 
+	else {
+		 dir = args[1];
 	}
+	d = opendir(dir);
+    if (d == NULL) {
+        perror("opendir() error");
+        return 1;
+    }
+	 while ((dir_entry = readdir(d)) != NULL) {
+        // Skip the "." and ".." entries
+        if (strcmp(dir_entry->d_name, ".") != 0 && strcmp(dir_entry->d_name, "..") != 0) {
+            printf("%s\n", dir_entry->d_name);
+        }
+    }
+
+    // Close the directory stream
+    if (closedir(d) == -1) {
+        perror("closedir() error");
+        return 1;
+    }
 
 	return 0;
 }
 
-int indus_cd(char **args) {
-	if (args[1] == NULL) {
-		// CD to home
+int	indus_cd(char **args) {
+	char* dir;
+
+	if (args[1] == NULL)
+		dir = currentUser.home;
+	else
+		dir = args[1];
+
+	if (chdir(dir) != 0) {
+		switch (errno) {
+			case ENOENT:
+				fprintf(stderr, "cd: %s: No such file or directory\n", dir);
+				break;
+			case ENOTDIR:
+				fprintf(stderr, "cd: %s: Not a directory\n", dir);
+				break;
+			case EACCES:
+				fprintf(stderr, "cd: %s: Permission denied\n", dir);
+				break;
+			default:
+				fprintf(stderr, "cd: Error occurred while changing directory to %s\n", dir);
+				break;
+		}
+		return 1;
 	}
 
 	return 0;
@@ -43,11 +100,16 @@ int indus_rm(char **args) {
 }
 
 int indus_help(char **args) {
-	if (args[1] == NULL) {
-		// show all builtins
-	}
-
-	return 0;
+	 if (args[1] == NULL) {
+        // Show all built-ins
+        printf("Available commands:\n");
+        for (int i = 0; i < num_builtins(); i++) {
+            printf("  %s\n", builtins_str[i]);
+        }
+    } else {
+        printf("No help available for '%s'.\n", args[1]);
+    }
+    return 0;
 }
 
 int indus_exit(char **args) { return 0; }
